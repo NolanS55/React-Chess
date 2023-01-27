ceb223beca448d96c43cde75ae57a7a61e64b234
NolanS55 <97527945+NolanS55@users.noreply.github.com>
2023-01-26 23:28:21 -0500

    initial

---
+++ src/Board.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const Board = () => {
+    return (  
+        <div className="chessBoard">
+            <div className="test">hi</div>
+            <div className="test">hi</div>
+            <div className="test">hi</div>
+            <div className="test">hi</div>
+        </div>
+    );
+}
+ 
+export default Board;
